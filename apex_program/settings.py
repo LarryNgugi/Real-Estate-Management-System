beccd397787c470a2acd553f66f2c3385f5a273b
larryNgugi <larry.josephgithaka@gmail.com>
2021-11-12 13:06:26 +0300

    Logins

--- apex_program/settings.py
+++ apex_program/settings.py
@@ -16,7 +16,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -51,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_auto_logout.middleware.auto_logout',
 ]
 
 ROOT_URLCONF = 'apex_program.urls'
@@ -73,7 +73,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'apex_program.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -81,10 +80,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-        
+
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -104,7 +102,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -118,7 +115,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
@@ -128,7 +124,6 @@ STATICFILES_DIR = [
     os.path.join(BASE_DIR, 'apex/static/')
 ]
 
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
@@ -136,13 +131,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
-
 AT_ENDPOINT_URL = "https://api.sandbox.africastalking.com/version1/messaging"
 # Live : https://api.africastalking.com/version1/messaging
 # Sandbox: https://api.sandbox.africastalking.com/version1/messaging
 
 AT_API_KEY = 'c2cc8290d7985d228c31c7384f6f763ce6f0cff58abe7ad1f79e27ba04551aba'
-
 
 AT_USER_NAME = 'sandbox'
 
@@ -151,3 +144,6 @@ AT_FROM_VALUE = '2390'
 LOGIN_URL = 'home'
 LOGIN_REDIRECT_URL = '/staff/profile'
 
+AUTO_LOGOUT = {'IDLE_TIME': 10,
+               'MESSAGE': 'The session has expired. Please login again to continue.'
+               }
